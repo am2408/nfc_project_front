52f89b5f0a3b07aafde5dd9d8cc4b8d53e6357eb
Salman-Ali05 <sallukhan0805@gmail.com>
2025-01-01 12:18:13 +0100

    Home base

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View, ImageBackground } from 'react-native';
+import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
+
+const image = require("./assets/background.jpg");
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container} edges={['left', 'right']}>
+        <ImageBackground style={styles.background} source={image}>
+          <View style={styles.container}>
+            <View style={styles.logoContainer}>
+              <Image style={styles.img} source={require("./assets/logo.png")} />
+              <Text>Mosquée Al Rahma</Text>
+            </View>
+          </View>
+        </ImageBackground>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  },
+  background: {
+    resizeMode: "cover",
+    flex: 1,
     justifyContent: 'center',
+  },
+  logoContainer: {
+    top: 100,
+    left: 35,
+    width: 125,
+    height: 125,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 5,
+    zIndex: 99,
+    backgroundColor : "white"
+  },
+  img: {
+    width: 100,
+    height: 100,
   },
 });

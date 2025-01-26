453c8364bcbe5c37baba046a06fee733a5f05053
Salman-Ali05 <sallukhan0805@gmail.com>
2025-01-26 13:49:35 +0100

    Revert "Merge branch 'abdoul' into ali"
    This reverts commit b4a378b738efb91bd211b30c93a62cc3d7e47d8b, reversing
    changes made to 74b15cf1ff4dba4b73068e47c541b8b4a1c025fa.

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,10 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    ['@babel/plugin-transform-class-properties', { loose: true }],
-    ['@babel/plugin-transform-private-methods', { loose: true }],
-    ['@babel/plugin-transform-private-property-in-object', { loose: true }],
-    'react-native-reanimated/plugin'
-  ]
-};
+    presets: [
+      "module:metro-react-native-babel-preset",
+      "@babel/preset-react" // Ajout du preset pour JSX
+    ],
+    plugins: [
+      "react-native-reanimated/plugin" // Pour react-native-reanimated si utilisé
+    ]
+  };
+  
